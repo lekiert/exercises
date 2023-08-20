c4e756244af133873a66564860f500c49676ebc3
lekiert <ekiert.lukasz@gmail.com>
2023-08-20 21:22:59 +0200

    added bootstrap

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -1,4 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  css: [
+    'bootstrap/dist/css/bootstrap.css',
+  ],
+  plugins: [
+    { src: '~/plugins/bootstrap.ts', mode: 'client' },
+  ]
 })
