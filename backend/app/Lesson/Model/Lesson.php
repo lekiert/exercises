e2a89a2fbf90451f3a21e80e3bb3dd04f133c306
lekiert <ekiert.lukasz@gmail.com>
2023-08-22 13:38:09 +0200

    added Lesson

---
+++ backend/app/Lesson/Model/Lesson.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Lesson\Model;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Lesson extends Model
+{
+    use HasFactory;
+}
