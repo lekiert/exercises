c94b85215c53c3d783e96bf1b1f34b41871487f7
lekiert <ekiert.lukasz@gmail.com>
2023-08-22 14:06:19 +0200

    fixed auth tests

--- backend/tests/Feature/Auth/RegistrationTest.php
+++ backend/tests/Feature/Auth/RegistrationTest.php
@@ -2,7 +2,8 @@
 
 test('new users can register', function () {
     $response = $this->post('/register', [
-        'name' => 'Test User',
+        'name' => 'John Doe',
+        'username' => 'johndoe1',
         'email' => 'test@example.com',
         'password' => 'password',
         'password_confirmation' => 'password',
