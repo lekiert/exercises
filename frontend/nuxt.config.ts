e98347600dbc0e7d1c9cc61ff2325aec92817d35
lekiert <ekiert.lukasz@gmail.com>
2023-11-12 21:00:32 +0100

    nuxt config

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -1,10 +1,26 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
+  imports: {
+    dirs: [
+      // Scan top-level modules
+      'composables',
+    ],
+  },
+  include: [
+    'types.d.ts',
+  ],
+  runtimeConfig: {
+    public: {
+      apiUrl: process.env.API_URL,
+    }
+  },
+  modules: [
+    '@pinia/nuxt',
+  ],
+  ssr: false,
   css: [
     'bootstrap/dist/css/bootstrap.css',
+    '~/assets/scss/main.css'
   ],
-  plugins: [
-    { src: '~/plugins/bootstrap.ts', mode: 'client' },
-  ]
 })
