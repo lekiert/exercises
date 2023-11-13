7391adedc7ad7a7b27f1a8bc704673dd673f496e
lekiert <ekiert.lukasz@gmail.com>
2023-11-13 11:32:35 +0100

    added exercises table component

---
+++ frontend/components/Admin/ExercisesTable.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+defineProps(['exercises']);
+</script>
+
+<template>
+  <table class="table" v-show="exercises.length">
+    <tbody>
+    <tr v-for="exercise in exercises">
+      <td>
+        <a :href="'/admin/exercise/' + exercise.id">{{ exercise.name }}</a></td>
+      <td>{{ exercise.created_at }}</td>
+    </tr>
+    </tbody>
+  </table>
+
+  <p v-show="!exercises.length" class="text-muted">Brak danych</p>
+</template>
