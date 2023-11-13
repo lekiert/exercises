573efd05e1db8b4a3819daf7c13addf7daa12de6
lekiert <ekiert.lukasz@gmail.com>
2023-11-13 11:31:24 +0100

    added working link to navbar

--- frontend/components/Navbar.vue
+++ frontend/components/Navbar.vue
@@ -8,15 +8,12 @@
       <div class="collapse navbar-collapse" id="navbarSupportedContent">
         <ul class="navbar-nav me-auto mb-2 mb-lg-0">
           <li class="nav-item">
-            <a class="nav-link active" aria-current="page" href="#">Home</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">Link</a>
+            <a class="nav-link active" aria-current="page" href="/admin/dashboard">Home</a>
           </li>
         </ul>
 
         <span class="navbar-text">
-          <a href="/logout">Logout</a>
+          <a href="/logout">Wyloguj</a>
         </span>
       </div>
     </div>
