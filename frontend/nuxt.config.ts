3960c824c7fea882c7c52ac7baf22df8b0dac6a6
lekiert <ekiert.lukasz@gmail.com>
2023-11-16 00:07:13 +0100

    added main.scss

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -21,6 +21,15 @@ export default defineNuxtConfig({
   ],
   ssr: false,
   css: [
-    '~/assets/scss/main.css'
+    '~/assets/scss/main.scss',
   ],
+  vite: {
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: '@use "@/assets/_colors.scss" as *;'
+        }
+      }
+    }
+  }
 })
