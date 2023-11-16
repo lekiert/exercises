1dd9523e26005429e6c6fbba38256e64e4934785
lekiert <ekiert.lukasz@gmail.com>
2023-11-16 21:49:57 +0100

    fix back link styling

--- frontend/components/Admin/BackLink.vue
+++ frontend/components/Admin/BackLink.vue
@@ -1,6 +1,12 @@
+<script lang="ts" setup>
+const props = defineProps(['to'])
+
+const go = () => navigateTo(props.to || '/admin/dashboard')
+</script>
+
 <template>
   <div class="mb-6">
-    <UButton to="/admin/dashboard">
+    <UButton @click="go">
       <UIcon class="i-heroicons-chevron-left" />
       Powrót
     </UButton>
